f180feba26e0442b6f4f0d4fa4858f8455576a5d
authorbeard <msnolan@gmail.com>
2018-09-02 11:46:06 -0400

    refactor first spec, begin second

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,16 +2,24 @@ require "spec_helper"
 
 RSpec.describe "./lib/game.rb" do 
   before(:each) do 
-    empty_board = (0..9).to_a.each_with_object({}) do | position, board|
+    empty_board = (0..8).to_a.each_with_object({}) do | position, board|
       board[position] = nil
     end
     allow(Board).to receive(:new).and_return(empty_board)
+    @game = Game.new
   end
 
   describe Game do
     it "initializes with an empty board" do 
-      expect(Game.new.board).to be_a(Hash)
+      expect(@game.board).to be_a(Hash)
+      expect(@game.board.keys.count).to eq 9
+      expect(@game.board.values.uniq.all?(&:nil?)).to be true
     end
+
+    it "initializes with two players" do 
+      expect(@game.players.count).to eq 2
+    end
+
 
   end
 end
