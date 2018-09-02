b87427c70da1594e0fc62204bb2b53d304688386
authorbeard <msnolan@gmail.com>
2018-09-02 12:43:36 -0400

    add specs for move validation

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -17,5 +17,20 @@ RSpec.describe "../lib/board.rb" do
 
       expect(board.state.values.all?(&:empty?)).to be false
     end
+
+    it "knows which board positions are valid" do 
+      board = Board.new
+      expect(board.valid_move?(10)).to be false
+    end
+
+    it "does not allow a move to an occupied space" do 
+      board = Board.new
+      board.state[5]="X"
+
+      expect {
+        board.state[5]="0"
+      }.not_to change(board, :state)
+
+    end
   end
 end
