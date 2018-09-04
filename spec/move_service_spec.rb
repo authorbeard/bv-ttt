bd5680aa4110cbe1f3023f509671a7c41352ad67
authorbeard <msnolan@gmail.com>
2018-09-04 10:06:40 -0400

    update move service specs

--- spec/move_service_spec.rb
+++ spec/move_service_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 RSpec.describe "../lib/move_service.rb" do 
   describe MoveService do 
     before :each do 
-      @svc      = MoveService.new
+      @board    = Board.new
       @player_x = double("player_x", piece: "X")
       @player_o = double("player_o", piece: "O")
     end
@@ -11,27 +11,27 @@ RSpec.describe "../lib/move_service.rb" do
     describe "#make_move" do    
       it "checks that the position is valid" do 
         expect_any_instance_of(Board).to receive(:valid_position?)
-        @svc.make_move(4, @player_x)
+        MoveService.make_move(@board, 4, @player_x)
       end
 
       it "checks that the position is unoccupied" do 
         expect_any_instance_of(Board).to receive(:space_is_available?)
-        @svc.make_move(4, @player_x)
+        MoveService.make_move(@board, 4, @player_x)
       end
 
       it "updates the board's state" do 
         expect_any_instance_of(Board).to receive(:update_state).with(4, @player_x.piece)
-        @svc.make_move(4, @player_x)
+        MoveService.make_move(@board, 4, @player_x)
       end
 
       it "returns true for valid moves" do 
-        expect(@svc.make_move(4, @player_o)).to eq true
+        expect(MoveService.make_move(@board, 4, @player_o)).to eq true
       end
 
       it "returns false for invalid moves" do 
-        @svc.make_move(7, @player_o)
-        expect(@svc.make_move(10, @player_o)).to be false
-        expect(@svc.make_move(7, @player_x)).to be false
+        MoveService.make_move(@board, 7, @player_o)
+        expect(MoveService.make_move(@board, 10, @player_o)).to be false
+        expect(MoveService.make_move(@board, 7, @player_x)).to be false
       end
     end
   end
